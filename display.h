b45c6ab4d449a7ba2e86db13f4d4d195a01c6c2d
DarkCybernetics <Dion.jacksoncs@yahoo.com>
2023-01-17 16:03:34 -0600

    Add files via upload

---
+++ display.h
@@ -0,0 +1,17 @@
+class Display
+{
+
+    public:
+        int Texture_Size = 16;
+        int Texture_Width = 512;
+        int Texture_Height = 512;
+        int Texture_UV_x = 0;
+        int Texture_UV_y = 0;
+
+    private:
+        void load_Texture(Texture_Height,Texture_Size,Texture_Width);
+        void load_file();
+    protected:
+
+
+}
